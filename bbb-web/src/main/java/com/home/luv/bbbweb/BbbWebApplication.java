c72b486f89d431e7fa48e4e442cb589fed20bfc6
unknown <lraheja@gmail.com>
2019-01-15 20:39:23 +0530

    First commit to dbranch

---
+++ bbb-web/src/main/java/com/home/luv/bbbweb/BbbWebApplication.java
@@ -0,0 +1,14 @@
+package com.home.luv.bbbweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbWebApplication.class, args);
+	}
+
+}
+
